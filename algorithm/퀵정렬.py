8e29e433985ec1978df98a3ed763ad6316da8cfe
100race <100race@naver.com>
2021-05-21 09:40:53 +0900

    2021.05.21 퀵정렬.py create

---
+++ algorithm/퀵정렬.py
@@ -0,0 +1,28 @@
+```python
+array = [5,7,9,0,3,1,6,2,4,8]
+
+def quick_sort(array, start, end):
+	if start >= end: #원소가 1개인 경우 종료
+		return
+	pivot = start #첫 원소를 피벗으로
+	left = start + 1
+	right = end
+	while(left <= right):
+		#피벗보다 큰 데이터를 찾을때까지 반복
+		while(left<=end and array[left] <= array[pivot]):
+			left += 1
+		#피벗보다 작은 데이터를 찾을때까지 반복
+		while(right>start and array[right] >= array[pivot]):
+			right -= 1
+		if(left > right): # 엇갈렸다면 작은 데이터와 피벗을 교체
+			array[right], array[pivot] = array[pivot], array[right]
+		else: #엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+			array[left], array[right] = array[right], array[left]
+	#분할 이후 왼쪽부분과 오른쪽 부분에서 각각 퀵 정렬 수행
+	quick_sort(array, start, right -1)
+	quick_sort(array, right +1, end)
+
+quick_sort(array, 0, len(array)-1)
+print(array)
+#실행결과
+#[0,1,2,3,4,5,6,7,8,9]
